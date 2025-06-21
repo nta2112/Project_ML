57b923685ec296a0fd9b16835f6f3da3bab979e4
leduong04 <119992125+leduong04@users.noreply.github.com>
2025-06-21 14:57:43 +0700

    Update Draft.py

--- Draft.py
+++ Draft.py
@@ -107,7 +107,7 @@ def predict_autoregressive(model, initial_context, prediction_steps, scaler, dev
 # Streamlit UI
 st.set_page_config(page_title="Stock Price Predictor", layout="wide")
 # st.title("📈 AI Stock Price Prediction (Strict Mode)")
-st.title("📈 AI Stock Price Prediction")
+st.title("📈 Lê Đoàn Dương AI Stock Price Prediction")
 
 # Sidebar controls
 with st.sidebar:
